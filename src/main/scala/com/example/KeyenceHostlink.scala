3ed83ec354e58b59e5720b5a2114a6131058e1f6
Naoto Shimazaki <nshimaza@cisco.com>
2018-08-13 12:47:58 +0900

    restore state saved in nodes.json

--- src/main/scala/com/example/KeyenceHostlink.scala
+++ src/main/scala/com/example/KeyenceHostlink.scala
@@ -1,35 +1,38 @@
 /**
- *
- * Copyright (c) 2018 Cisco and/or its affiliates.
- *
- * This software is licensed to you under the terms of the Cisco Sample
- * Code License, Version 1.0 (the "License"). You may obtain a copy of the
- * License at
- *
- *                https://developer.cisco.com/docs/licenses
- *
- * All use of the material herein must be in accordance with the terms of
- * the License. All rights not expressly granted by the License are
- * reserved. Unless required by applicable law or agreed to separately in
- * writing, software distributed under the License is distributed on an "AS
- * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied.
- */
+  *
+  * Copyright (c) 2018 Cisco and/or its affiliates.
+  *
+  * This software is licensed to you under the terms of the Cisco Sample
+  * Code License, Version 1.0 (the "License"). You may obtain a copy of the
+  * License at
+  *
+  * https://developer.cisco.com/docs/licenses
+  *
+  * All use of the material herein must be in accordance with the terms of
+  * the License. All rights not expressly granted by the License are
+  * reserved. Unless required by applicable law or agreed to separately in
+  * writing, software distributed under the License is distributed on an "AS
+  * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+  * or implied.
+  */
 
 package com.example
+
+import java.util
 
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream._
 import akka.stream.scaladsl.{Broadcast, Framing, GraphDSL, Keep, RestartSource, Sink, Source, Tcp, Zip}
 import akka.util.ByteString
-import org.dsa.iot.dslink.node.Permission
+import org.dsa.iot.dslink.node.{Node, Permission}
 import org.dsa.iot.dslink.node.actions.table.Row
 import org.dsa.iot.dslink.node.actions.{Action, ActionResult, Parameter}
 import org.dsa.iot.dslink.node.value.{Value, ValueType}
 import org.dsa.iot.dslink.{DSLink, DSLinkFactory, DSLinkHandler}
 import org.slf4j.LoggerFactory
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
@@ -92,81 +95,6 @@ class KeyenceHostLinkDSLinkHandler() extends DSLinkHandler {
                 .setValueType(ValueType.NUMBER)
                 .setValue(new Value(0))
                 .build()
-              plcNode.createChild("Start Polling", true)
-                .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
-                  val plcNode = event.getNode.getParent
-                  val startPollParams = for {
-                    interval <- (runningPlcMap.get(plcNode.getName) match {
-                      case None => Right(())
-                      case Some(_) => Left("Polling already started")
-                    })
-                      .flatMap(_ => Right(event.getParameter("Interval (ms)", new Value(500)))
-                        .flatMap(v => if (v.getType != ValueType.NUMBER) Left("Interval must be number") else Right(v.getNumber.intValue))
-                        .flatMap(i => if (i < 10) Left("Interval must not be less than 10ms") else Right(i)))
-                  } yield interval
-                  startPollParams match {
-                    case Right(interval) =>
-                      val pollingStreamSource = Source.fromGraph(GraphDSL.create() { implicit builder =>
-                        import GraphDSL.Implicits._
-                        val in = Source.tick(1.second, interval.milliseconds, ByteString(s"RDS $devName$devAddr $count\r\n"))
-                        val plc = Tcp().outgoingConnection("localhost", 8501)
-                          .via(Framing.delimiter(ByteString("\r\n"), 30000))
-                        val bcast = builder.add(Broadcast[ByteString](2, true))
-                        val zip: FanInShape2[ByteString, ByteString, (ByteString, ByteString)] = builder.add(Zip[ByteString, ByteString])
-                        // @formatter:off
-                        in ~> bcast ~> plc ~> zip.in0
-                              bcast ~>        zip.in1
-                        // @formatter:on
-                        SourceShape(zip.out)
-                      })
-                      val pollingStream = RestartSource.withBackoff(1.seconds, 60.seconds, 0.1) { () => pollingStreamSource }
-                        .viaMat(KillSwitches.single)(Keep.right)
-                        .toMat(Sink.foreach { response =>
-                          val children = (devAddr until devAddr + count).map(i => plcNode.getChild(devName, true).getChild(i.toString, true))
-                          val str = response._1.utf8String
-                          plcNode.getChild("CSV", true).setValue(new Value(str.replace(' ', ',')))
-                          str.split(' ').map(_.toInt).zip(children).foreach { t => t._2.setValue(new Value(t._1)) }
-                        })(Keep.both).run()
-                      runningPlcMap = runningPlcMap + (name -> pollingStream)
-                      plcNode.getChild("isRunning", true).setValue(new Value(true))
-                      plcNode.getChild("pollingInterval", true).setValue(new Value(interval))
-                      event.getTable.addRow(Row.make(new Value("Success")))
-
-                    case Left(err) => event.getTable.addRow(Row.make(new Value(err)))
-                  }
-                }).addParameter(new Parameter("Interval (ms)", ValueType.NUMBER, new Value(500))
-                  .setDescription("Polling interval in millisecond"))
-                  .addResult(new Parameter("Result", ValueType.STRING))
-                )
-                .build()
-              plcNode.createChild("Stop Polling", true)
-                .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
-                  val plcNode = event.getNode.getParent
-
-                  runningPlcMap.get(plcNode.getName) match {
-                    case None => event.getTable.addRow(Row.make(new Value("Polling already stopped")))
-                    case Some((killSwitch, _)) =>
-                      killSwitch.shutdown()
-                      runningPlcMap = runningPlcMap - name
-                      plcNode.getChild("isRunning", true).setValue(new Value(false))
-                      plcNode.getChild("pollingInterval", true).setValue(new Value(0))
-                      event.getTable.addRow(Row.make(new Value("Success")))
-                  }
-                }).addResult(new Parameter("Result", ValueType.STRING))
-                )
-                .build()
-              plcNode.createChild("Remove", true)
-                .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
-                  val isRunning = event.getNode.getParent.getChild("isRunning", true)
-                  if (isRunning.getValue.getBool == true) {
-                    event.getTable.addRow(Row.make(new Value("Stop Polling before remove")))
-                  } else {
-                    event.getNode.getParent.delete(true)
-                    event.getTable.addRow(Row.make(new Value("Success")))
-                  }
-                }).addResult(new Parameter("Result", ValueType.STRING))
-                )
-                .build()
               val device = plcNode.createChild(devName, true).build()
               for (i <- devAddr until devAddr + count) {
                 device.createChild(i.toString, true)
@@ -174,6 +102,9 @@ class KeyenceHostLinkDSLinkHandler() extends DSLinkHandler {
                   .setValue(new Value(0))
                   .build()
               }
+              addStartPollingAction(plcNode)
+              addStopPollingAction(plcNode)
+              addRemoveAction(plcNode)
               plcNode.createChild("CSV", true).setValueType(ValueType.STRING).setValue(new Value("")).build
               event.getTable.addRow(Row.make(new Value("Success")))
             }
@@ -199,6 +130,111 @@ class KeyenceHostLinkDSLinkHandler() extends DSLinkHandler {
         .addResult(new Parameter("Result", ValueType.STRING))
       )
       .build()
+
+    def addStartPollingAction(plcNode: Node) = {
+      plcNode.createChild("Start Polling", true)
+        .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
+          val name = plcNode.getName
+          val startPollParams = for {
+            interval <- (runningPlcMap.get(name) match {
+              case None => Right(())
+              case Some(_) => Left("Polling already started")
+            })
+              .flatMap(_ => Right(event.getParameter("Interval (ms)", new Value(500)))
+                .flatMap(v => if (v.getType != ValueType.NUMBER) Left("Interval must be number") else Right(v.getNumber.intValue))
+                .flatMap(i => if (i < 10) Left("Interval must not be less than 10ms") else Right(i)))
+          } yield interval
+          startPollParams match {
+            case Right(interval) =>
+              runningPlcMap = runningPlcMap + (name -> createPollingStream(plcNode, interval))
+              plcNode.getChild("isRunning", true).setValue(new Value(true))
+              plcNode.getChild("pollingInterval", true).setValue(new Value(interval))
+              event.getTable.addRow(Row.make(new Value("Success")))
+
+            case Left(err) => event.getTable.addRow(Row.make(new Value(err)))
+          }
+        }).addParameter(new Parameter("Interval (ms)", ValueType.NUMBER, new Value(500))
+          .setDescription("Polling interval in millisecond"))
+          .addResult(new Parameter("Result", ValueType.STRING))
+        )
+        .build()
+    }
+
+    def addStopPollingAction(plcNode: Node) = {
+      plcNode.createChild("Stop Polling", true)
+        .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
+          val name = plcNode.getName
+          runningPlcMap.get(name) match {
+            case None => event.getTable.addRow(Row.make(new Value("Polling already stopped")))
+            case Some((killSwitch, _)) =>
+              killSwitch.shutdown()
+              runningPlcMap = runningPlcMap - name
+              plcNode.getChild("isRunning", true).setValue(new Value(false))
+              plcNode.getChild("pollingInterval", true).setValue(new Value(0))
+              event.getTable.addRow(Row.make(new Value("Success")))
+          }
+        }).addResult(new Parameter("Result", ValueType.STRING))
+        )
+        .build()
+    }
+
+    def addRemoveAction(plcNode: Node) = {
+      plcNode.createChild("Remove", true)
+        .setAction(new Action(Permission.CONFIG, (event: ActionResult) => {
+          val isRunning = event.getNode.getParent.getChild("isRunning", true)
+          if (isRunning.getValue.getBool == true) {
+            event.getTable.addRow(Row.make(new Value("Stop Polling before remove")))
+          } else {
+            event.getNode.getParent.delete(true)
+            event.getTable.addRow(Row.make(new Value("Success")))
+          }
+        }).addResult(new Parameter("Result", ValueType.STRING))
+        )
+        .build()
+    }
+
+    def createPollingStream(plcNode: Node, interval: Int): (UniqueKillSwitch, Future[Done]) = {
+      val ipAddr = plcNode.getAttribute("ipAddr").getString
+      val port = plcNode.getAttribute("port").getNumber.intValue
+      val devName = plcNode.getAttribute("devName").getString
+      val devAddr = plcNode.getAttribute("devAddr").getNumber.intValue
+      val count = plcNode.getAttribute("count").getNumber.intValue
+      val pollingStreamSource = Source.fromGraph(GraphDSL.create() { implicit builder =>
+        import GraphDSL.Implicits._
+        val in = Source.tick(1.second, interval.milliseconds, ByteString(s"RDS $devName$devAddr $count\r\n"))
+        val plc = Tcp().outgoingConnection(ipAddr, 8501)
+          .via(Framing.delimiter(ByteString("\r\n"), 30000))
+        val bcast = builder.add(Broadcast[ByteString](2, true))
+        val zip: FanInShape2[ByteString, ByteString, (ByteString, ByteString)] = builder.add(Zip[ByteString, ByteString])
+        // @formatter:off
+        in ~> bcast ~> plc ~> zip.in0
+              bcast ~>        zip.in1
+        // @formatter:on
+        SourceShape(zip.out)
+      })
+      RestartSource.withBackoff(1.seconds, 60.seconds, 0.1) { () => pollingStreamSource }
+        .viaMat(KillSwitches.single)(Keep.right)
+        .toMat(Sink.foreach { response =>
+          val children = (devAddr until devAddr + count).map(i => plcNode.getChild(devName, true).getChild(i.toString, true))
+          val str = response._1.utf8String
+          plcNode.getChild("CSV", true).setValue(new Value(str.replace(' ', ',')))
+          str.split(' ').map(_.toInt).zip(children).foreach { t => t._2.setValue(new Value(t._1)) }
+        })(Keep.both).run()
+    }
+
+    superRoot.getChildren.values.asScala.toSeq
+      .filter(_.getName != "addPLC").filter(_.getName != "sys")
+      .map { plcNode =>
+        println(plcNode.getName)
+        addStartPollingAction(plcNode)
+        addStopPollingAction(plcNode)
+        addRemoveAction(plcNode)
+        plcNode
+      }.filter(_.getChild("isRunning", true).getValue.getBool)
+      .foreach { plcNode =>
+        val interval = plcNode.getChild("pollingInterval", true).getValue.getNumber.intValue
+        runningPlcMap = runningPlcMap + (plcNode.getName -> createPollingStream(plcNode, interval))
+      }
   }
 
   override def onResponderConnected(link: DSLink): Unit = {
