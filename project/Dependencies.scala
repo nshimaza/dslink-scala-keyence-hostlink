5eda299b208072fec1e9304a16c109109a4fbc9d
Naoto Shimazaki <naoto.shimazaki@gmail.com>
2019-05-09 20:14:12 +0900

    catch up to latest dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val dsLink = "org.iot-dsa" % "dslink" % "0.18.5"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
-//  lazy val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.5.12"
-  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.14"
+  lazy val dsLink = "org.iot-dsa" % "dslink" % "0.20.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % "2.5.22"
 }
